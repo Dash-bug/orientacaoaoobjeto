f2a13a20ba9965818d8c784ae91e1ad1a0aa3dca
Dash-bug <darlan_schmidt@estudante.sc.senai.br>
2021-04-16 11:23:03 -0300

    primeiro commit POO

---
+++ src/Funcionario.java
@@ -0,0 +1,26 @@
+
+public class Funcionario {
+    
+    public Funcionario(String nome, String departamento, double salario, String RG,String Dataentrada) {
+        
+        this.nome = nome;
+        this.departamento = departamento;
+        this.salario = salario;
+        this.RG = RG;
+        this.Dataentrada = Dataentrada;
+        
+    }
+    
+     String nome;
+     String departamento;
+     double salario;
+     String RG;
+     String Dataentrada;
+     
+        
+    public void recebeAumento(double aumento) {
+        
+
+    }
+    
+}
---
+++ src/aaa/Aaa.java
@@ -0,0 +1,15 @@
+
+package aaa;
+
+public class Aaa {
+
+    
+    public static void main(String[] args) {
+        
+        Funcionario f1 = new funcionario();
+        f1.nome
+        f1.salario
+        
+    }
+    
+}
